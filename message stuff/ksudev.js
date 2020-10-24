f8ed83de9ba2100333125e56fd51a03c835f7733
Ancheng <anchengbi21@gmail.com>
2020-10-24 14:23:38 -0400

    poop

--- message stuff/ksudev.js
+++ message stuff/ksudev.js
@@ -16,4 +16,30 @@ client.on('message', msg=> {
     }
 }
 );
+client.on('message', message => {
+    if (!message.guild) return;
+ 
+   if (message.content.startsWith('!kick')) {
+     const user = message.mentions.users.first();
+     if (user) {
+       const member = message.guild.member(user);
+       if (member) {
+         member
+           .kick('Optional reason that will display in the audit logs')
+           .then(() => {
+             message.reply(`kicked ${user.tag}`);
+           })
+           .catch(err => {
+             message.reply('missing perms/error');
+             console.error(err);
+           });
+       } else {
+         // The mentioned user isn't in this guild
+         message.reply("user does not exist here");
+       }
+      } else {
+       message.reply("err: no one was mentioned");
+     }
+   }
+ });
 client.login('NzQwNjE3OTk5OTQ1MzAyMDk5.XyroQw.kwLjXM6HDn2JnS-B_qHfNuc74as');
