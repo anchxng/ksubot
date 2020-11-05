77640c1bb140db5f9d98128ec74fc6ee646cbb4a
Ancheng <anchengbi21@gmail.com>
2020-11-05 09:11:07 -0500

    coc

---
+++ message stuff/admin/ban.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// on sequence
+client.on('ready', () => {
+  console.log(`robot: on`);
+});
+
+client.login('NzQwNjE3OTk5OTQ1MzAyMDk5.XyroQw.1wRpH5-W3ts0mj9RSqiV5xbrF7M');
