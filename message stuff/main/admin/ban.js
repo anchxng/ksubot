03630df1f0c2e00d27053f68b247829bc13b4b2e
Ancheng <anchengbi21@gmail.com>
2020-11-15 17:45:00 -0500

    d

--- message stuff/main/admin/ban.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-// on sequence
-client.on('ready', () => {
-  console.log(`robot: on`);
-});
-
-client.login('NzQwNjE3OTk5OTQ1MzAyMDk5.XyroQw.5220O-nkjoXChxHOA4l357MiSms');
--- message stuff/main/ksudev.js
+++ message stuff/main/ksudev.js
@@ -69,6 +69,33 @@ client.on('message', message => {
     }
   }
 });
+// kick
+client.on('message', message => {
+  if (!message.guild) return;
+
+ if (message.content.startsWith('!kick')) {
+   const user = message.mentions.users.first();
+   if (user) {
+     const member = message.guild.member(user);
+     if (member) {
+       member
+         .kick('Optional reason that will display in the audit logs')
+         .then(() => {
+           message.reply( ```succes```);
+         })
+         .catch(err => {
+           message.reply(' ```missing perms/error``` ');
+           console.error(err);
+         });
+     } else {
+       // The mentioned user isn't in this guild
+       message.reply(" ```user does not exist here``` ");
+     }
+    } else {
+     message.reply(" ```err: no one was mentioned``` ");
+   }
+ }
+});
 // zzz
 client.on('message', msg => {
   if (msg.content === 'zzz'){
