5dd00d624bf6e97e66b0c5c47ad53b3b92117b49
Ancheng <anchengbi21@gmail.com>
2020-11-20 21:18:57 -0500

    a

--- main/egg.js
+++ main/egg.js
@@ -22,4 +22,4 @@ client.on('message', msg => {
 	}
 });
 
-client.login(process.env.EGG_BOT_TOKEN = '');
+client.login(process.env.EGG_BOT_TOKEN = 'NzQwNjE3OTk5OTQ1MzAyMDk5.XyroQw.p-hxadM_-70h_C5cmFV25I-U-P0');
--- vc/join.js
+++ vc/join.js
@@ -9,4 +9,4 @@ client.on('message', async message => {
       }
     }
   });
- client.login('tokenhere')
+ client.login('NzQwNjE3OTk5OTQ1MzAyMDk5.XyroQw.p-hxadM_-70h_C5cmFV25I-U-P0')
