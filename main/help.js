9cb06d81e9a7cc4754a98e0348f92f99b7005f27
Ancheng <anchengbi21@gmail.com>
2020-11-23 07:39:35 -0500

    j

--- main/help.js
+++ main/help.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log('robot on');
+});
+
 client.on('message', message => {
     if (message.content === 'embed') {
       const embed = new MessageEmbed()
@@ -14,3 +18,5 @@ client.on('message', message => {
       message.channel.send(embed);
     }
   });
+
+  client.login('NzQwNjE3OTk5OTQ1MzAyMDk5.XyroQw.p-hxadM_-70h_C5cmFV25I-U-P0');
