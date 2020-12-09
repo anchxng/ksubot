459c278e3f16b7a6fa873b7adca64e368ea753bb
ksu-tech <anchengbi21@gmail.com>
2020-12-09 13:05:54 -0500

    jjj

--- src/main/jokes.js
+++ src/main/jokes.js
@@ -7,4 +7,8 @@ client.on('message', message => {
       msg.reply(Joke.getRandomJoke)
     }
 });
+ƒ (); {
+  var i = randomNumber(0, arr.length - 1);
+  return arr[i].joke;
+};
 client.login('NzQwNjE3OTk5OTQ1MzAyMDk5.XyroQw.CB1CgycWXcAGnK8NqvqcruuQvEA');
